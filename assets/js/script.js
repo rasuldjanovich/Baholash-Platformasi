106c169b0a5c79d9797ac1d76d24a3c1e8047580
Abdulqodir <rasuldjanovich7@gmail.com>
2023-08-10 12:16:13 +0300

    first commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,7 +19,7 @@ window.addEventListener("scroll", function () {
   let header = document.querySelector("nav");
   if (this.window.scrollY > 0) {
     header.classList.add("scrolled")
-    header.classList.add("logos")
+    // header.classList.add("logos")
   } else {
     header.classList.remove("scrolled")
     // header.classList.remove("logos")
